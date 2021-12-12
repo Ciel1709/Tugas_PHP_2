5ca8a345f929f7bc5bbcfc536e94eba88770a4bf
Ciel <danagames30@gmail.com>
2021-12-12 18:59:30 +0700

    Initial commit

---
+++ tugas2_1.php
@@ -0,0 +1,11 @@
+<?php 
+    $hari1 = 36;
+    $hari2 = "X";
+    $pekerja1 = 12;
+    $pekerja2 = 24;
+    echo $hari1." Hari = ".$pekerja1." Pekerja <br>";
+    echo $hari2." Hari = ".$pekerja2." Pekerja <br>";
+    echo "X = ...? <br>";
+    $jawaban = $pekerja1/$pekerja2*$hari1;
+    echo "Jawaban : ".$jawaban;
+?>
---
+++ tugas2_2.php
@@ -0,0 +1,9 @@
+<?php 
+    $jual = 80000;
+    $beli = 50000;
+    $keuntungan = ($jual-$beli)/$beli*100;
+    echo "Jual = ".$jual."<br>";
+    echo "Beli = ".$beli."<br>";
+    echo "Keuntungan = ? <br>";
+    echo "Jawaban: ".$keuntungan."%";
+?>
